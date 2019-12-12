3449c90dd01969a426275b9c46e548d34db6e340
lavanya-baskar <lavanyalava2511@gmail.com>
2019-12-12 15:51:19 +0530

    #dec12

---
+++ lavanya/Factorial.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+class Factorial
+{
+	public static void main(String[] args) {
+		int i,n,fact=1;
+		System.out.println("n:");
+		Scanner inp = new Scanner(System.in);
+		n=inp.nextInt();
+		for(i=1;i<=n;i++)
+		{
+			fact*=i;
+		}
+
+System.out.println(fact);
+		
+	}
+}
