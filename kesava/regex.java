940e6e9d0bc83a5b8591694e3b15b7b207299b69
kesavamurthy-kandeeban <kesavan.k2014@gmail.com>
2019-12-12 15:08:18 +0530

    #Regex

---
+++ kesava/regex.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Regex {
+
+
+    public static void main(String[] args) {
+
+        int max=0;
+        Scanner inp=new Scanner(System.in);
+        String a=inp.nextLine();
+        String[] arr=a.split("[\\s@#$%^&*()!123456789]");
+        for(String s:arr)
+        {
+
+            if(s.length()>max)
+            {
+                max=s.length();
+            }
+
+        }
+        System.out.println(max);
+    }
+}
