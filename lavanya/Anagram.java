643616326eb8dfff7dbcd8b7a6b80bc93cecc5c5
lavanya <lavanyalava2511@gmail.com>
2019-12-13 17:17:20 +0530

    # dec-13

--- lavanya/Anagram.java
+++ lavanya/Anagram.java
@@ -8,11 +8,14 @@ class Anagram
     String str1=inp.next();
     System.out.println("enter 2nd string:");
     String str2=inp.next();
+    //convert string into character and store in array
     char[]ArrS1= str1.toLowerCase().toCharArray();
     char[]ArrS2= str2.toLowerCase().toCharArray();
+    //to sort the character in array
     Arrays.sort(ArrS1);
     Arrays.sort(ArrS2);
     boolean anagram=Arrays.equals(ArrS1,ArrS2);
+    //to check whether anagram or not
     if(anagram)
     {
       System.out.println("Anagram");
