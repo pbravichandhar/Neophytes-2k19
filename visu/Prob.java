52ec2175c5f67bba12bd1bac26c47166113419d7
“visu231” <visusekar@calibraint.com>
2019-12-13 17:25:29 +0530

    dec13programs

---
+++ visu/Prob.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Prob
+{
+    public static void main(String args[])
+    {
+        int i,d,n,r,x,ch=0;
+        float cir,da;
+        Scanner sc=new Scanner(System.in);
+        d=sc.nextInt();
+        for(i=1;i<=d;i++)
+        {
+            r=sc.nextInt();
+            x=sc.nextInt();
+            cir=22/7*2*r;
+            da=100*x;
+            if(da>=cir)
+            {
+                ch++;
+            }
+            
+        }
+        System.out.println(ch);
+    }
+}
