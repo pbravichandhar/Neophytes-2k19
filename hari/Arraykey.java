37c4592e35e94ead13cd38b8e0541260833d2517
Harishankar-26 <shankar26udt@gmail.com>
2019-12-19 17:34:02 +0530

    #Arraykey using 2 loops

---
+++ hari/Arraykey.java
@@ -0,0 +1,30 @@
+import java.lang.*;
+import java.util.Scanner;
+class Arraykey
+{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int key=s.nextInt();
+        int n=s.nextInt();
+        int[] arr=new int[n];
+        int a,b;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            a=arr[i];
+            while(a!=0)
+            {
+            b=a%10;
+            a=a/10;
+            if(key==b)
+            {
+                System.out.print("Yes");
+                return;
+            }
+            }
+        }
+    }
+}
