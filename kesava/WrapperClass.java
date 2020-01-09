664881107a9298968535db98ef72c58ddfe5aaf6
kesavamurthy-kandeeban <kesavan.k2014@gmail.com>
2020-01-09 10:02:34 +0530

    #reupload

--- kesava/WrapperClass.java
+++ kesava/WrapperClass.java
@@ -1,21 +1,20 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-public class WrapperClass
-{
-    public static void main(String []args)}
-    {
-        //Converting Primitive Datatype into Object
-          Integer [] a= new Integer[10];
-          Scanner s=new Scanner(System.in);
-          for(int i=0;i<10;i++)
-          {
-              a[i]=s.nextInt();
-          }
-          ArrayList<Integer> ShowList=new ArrayList<>();
-          for(int item:a)
-            {
-               ShowList.add(item);
-            }
-          System.out.println(ShowList);
+
+public class WrapperClass {
+  public static void main(String []args)
+
+  {
+    // Converting Primitive Datatype into Object
+    Integer[] a = new Integer[10];
+    Scanner s = new Scanner(System.in);
+    for (int i = 0; i < 10; i++) {
+      a[i] = s.nextInt();
     }
+    ArrayList<Integer> ShowList = new ArrayList<>();
+    for (int item : a) {
+      ShowList.add(item);
+    }
+    System.out.println(ShowList);
+  }
 }
